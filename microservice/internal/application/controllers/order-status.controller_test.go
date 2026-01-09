d86fe97819d8572854eca1a7bdd69d3e521e9350
Wesley Kaihara <wesleykaihara@gmail.com>
2026-01-09 00:09:21 -0300

    Resolve lint error

--- microservice/internal/application/controllers/order-status.controller_test.go
+++ microservice/internal/application/controllers/order-status.controller_test.go
@@ -14,6 +14,7 @@ func TestNewOrderStatusController(t *testing.T) {
 
 	if controller == nil {
 		t.Error("Expected controller to be created, got nil")
+		return
 	}
 
 	if controller.dataSource != mockOrderStatusDS {
